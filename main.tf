7909d52669655ada639f9bd1e80dd9560ce03380
jhuttley-F5-com <72359774+jhuttley-F5-com@users.noreply.github.com>
2024-05-07 14:13:22 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-#  version = "19.15.3"
+  version = "19.15.3"
 
   cluster_name    = local.cluster_name
 #  cluster_version = "1.28"
